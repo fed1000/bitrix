0eb0d8d64b5b6479f60b3f4de6b5ebd6098f49ba
Fedot Shmakov <shmakov@gigansk.ru>
2021-12-13 10:57:25 +0700

    [ex1-11] main include new template

--- local/templates/exam1/footer.php
+++ local/templates/exam1/footer.php
@@ -29,12 +29,13 @@ IncludeTemplateLangFile(__FILE__);
 						<!-- /side menu -->
 					<?}?>
                     <!-- side anonse -->
-                    <div class="side-block side-anonse">
-                        <div class="title-block"><span class="i i-title01"></span>Полезная информация!</div>
-                        <div class="item">
-                            <p>Клиенты предпочитают все больше эко-материалов.</p>
-                        </div>
-                    </div>
+                    <?$APPLICATION->IncludeComponent("bitrix:main.include","section_inform",Array(
+                            "AREA_FILE_SHOW" => "sect", 
+                            "AREA_FILE_SUFFIX" => "inc", 
+                            //"AREA_FILE_RECURSIVE" => "Y", 
+                            "EDIT_TEMPLATE" => "" 
+                        )
+                    );?>
                     <!-- /side anonse -->
                     <!-- side wrap -->
  					<div class="side-wrap">
